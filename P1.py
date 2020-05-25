bb0c1e00190e1b2370e568fcef3445341bef3234
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-25 11:07:34 -0400

    Added helper methods. Reformatted files

--- P1.py
+++ P1.py
@@ -46,25 +46,26 @@
 # In[1]:
 
 
-#importing some useful packages
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
 import cv2
-get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.image as mpimg
+# importing some useful packages
+import matplotlib.pyplot as plt
+import numpy as np
 
+get_ipython().run_line_magic('matplotlib', 'inline')
 
 # ## Read in an Image
 
 # In[2]:
 
 
-#reading in an image
+# reading in an image
 image = mpimg.imread('test_images/solidWhiteRight.jpg')
 
-#printing out some stats and plotting
+# printing out some stats and plotting
 print('This image is:', type(image), 'with dimensions:', image.shape)
-plt.imshow(image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
+plt.imshow(
+    image)  # if you wanted to show a single color channel image called 'gray', for example, call as plt.imshow(gray, cmap='gray')
 
 
 # ## Ideas for Lane Detection Pipeline
@@ -88,10 +89,103 @@ plt.imshow(image)  # if you wanted to show a single color channel image called '
 # In[3]:
 
 
-import math
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Or use BGR2GRAY if you read an image with cv2.imread()
+    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
 
+
+def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    `vertices` should be a numpy array of integer points.
+    """
+    # defining a blank mask to start with
+    mask = np.zeros_like(img)
+
+    # defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    # filling pixels inside the polygon defined by "vertices" with the fill color
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+
+    # returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+        
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len,
+                            maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    return line_img, lines
+
+
+# Python 3 has support for cool math symbols.
+
+def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
+    
+    `initial_img` should be the image before any processing.
+    
+    The result image is computed as follows:
+    
+    initial_img * α + img * β + γ
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, α, img, β, γ)
 
 
 # ## Test Images
@@ -103,8 +197,8 @@ import math
 
 
 import os
-os.listdir("test_images/")
 
+os.listdir("test_images/")
 
 # ## Build a Lane Finding Pipeline
 # 
@@ -137,10 +231,9 @@ max_angle = .9
 # In[6]:
 
 
-def draw_line(image, slope, intercept, x_a, x_b, color=(0,255,0), thickness=3):
+def draw_line(image, slope, intercept, x_a, x_b, color=(0, 255, 0), thickness=3):
     return cv2.line(image, (int(x_a), int(slope * x_a + intercept)),
-                          (int(x_b), int(slope * x_b + intercept)), color, thickness)
-    
+                    (int(x_b), int(slope * x_b + intercept)), color, thickness)
 
 
 # In[7]:
@@ -158,36 +251,35 @@ def lane_finder(image, draw_on=True):
 
     copped_image = region_of_interest(canny_points, [vertices])
 
-    hough, lines = hough_lines(copped_image, rho,theta, threshold, max_line_length, max_line_gap)
+    hough, lines = hough_lines(copped_image, rho, theta, threshold, max_line_length, max_line_gap)
 
     slope = (lines[:, :, 1] - lines[:, :, 3]) / (lines[:, :, 0] - lines[:, :, 2])
-    
-    mask = np.logical_and(np.abs(slope) > min_angle, np.abs(slope) <max_angle)
+
+    mask = np.logical_and(np.abs(slope) > min_angle, np.abs(slope) < max_angle)
     mask = np.logical_and(mask, np.isfinite(slope))
     mask = mask.flatten()
-    
-    
+
     slope = slope[mask]
     lines = lines[mask]
-    
+
     intercept = lines[:, :, 1] - (slope * lines[:, :, 0])
     left_lane = slope < 0
     average_slope_left = np.average(slope[left_lane])
     average_intercept_left = np.average(intercept[left_lane])
     average_slope_right = np.average(slope[np.logical_not(left_lane)])
     average_intercept_right = np.average(intercept[np.logical_not(left_lane)])
-    
+
     if draw_on:
-        print(average_slope_left,average_intercept_left,  average_slope_right, average_intercept_right)
+        #         print(average_slope_left,average_intercept_left,  average_slope_right, average_intercept_right)
 
         output = np.copy(image)
 
-        output = draw_line(output,average_slope_left, average_intercept_left, 0, width/2 )
-        output = draw_line(output,average_slope_right, average_intercept_right,width / 2, width )
+        output = draw_line(output, average_slope_left, average_intercept_left, 0, width / 2)
+        output = draw_line(output, average_slope_right, average_intercept_right, width / 2, width)
 
-    #     color_edges = np.dstack((copped_image, copped_image, copped_image))
-    #     output = weighted_img(color_edges, hough)
-    #     output = cv2.polylines(output, [vertices], True, (0, 255, 255), 4)
+        #     color_edges = np.dstack((copped_image, copped_image, copped_image))
+        #     output = weighted_img(color_edges, hough)
+        #     output = cv2.polylines(output, [vertices], True, (0, 255, 255), 4)
         return output
     else:
         return average_slope_left, average_intercept_left, average_slope_right, average_intercept_right
@@ -200,12 +292,10 @@ output_folder = 'test_images_output'
 
 for file in os.listdir("test_images/"):
     print(file)
-    image= mpimg.imread(f'test_images/{file}')
-    output  = lane_finder(image)
-    
-    cv2.imwrite(f'{output_folder}/{file}',output)
-    
+    image = mpimg.imread(f'test_images/{file}')
+    output = lane_finder(image)
 
+    cv2.imwrite(f'{output_folder}/{file}', output)
 
 # ## Test on Videos
 # 
@@ -234,7 +324,6 @@ for file in os.listdir("test_images/"):
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
 
-
 # In[10]:
 
 
@@ -245,17 +334,18 @@ N = 10
 
 
 def running_mean(x, N):
-    cumsum = np.cumsum(x, axis=0) 
-#     print((cumsum[N:] - cumsum[:-N]) / float(N))
+    cumsum = np.cumsum(x, axis=0)
+    #     print((cumsum[N:] - cumsum[:-N]) / float(N))
     return ((cumsum[N:] - cumsum[:-N]) / float(N))[-1]
 
 
 # In[12]:
 
 
-values = np.array([[]]).reshape((0,4))
+values = np.array([[]]).reshape((0, 4))
 
 i = 0
+
 
 def process_image(image):
     global values, i
@@ -263,21 +353,21 @@ def process_image(image):
     # TODO: put your pipeline here,
     # you should return the final output (image where lines are drawn on lanes)
     lines = lane_finder(image, False)
-    _, width,_ = image.shape
-    
+    _, width, _ = image.shape
+
     values = np.concatenate((values, [lines]))
-    
+
     if i >= N:
         m_l, b_l, m_r, b_r = running_mean(values, N)
     else:
         m_l, b_l, m_r, b_r = lines
-        
+
     output = np.copy(image)
-    output = draw_line(output,m_l, b_l, 0, width/2 )
-    output = draw_line(output,m_r, b_r,width / 2, width )
-    
-    i+= 1
-    
+    output = draw_line(output, m_l, b_l, 0, width / 2)
+    output = draw_line(output, m_r, b_r, width / 2, width)
+
+    i += 1
+
     return output
 
 
@@ -293,9 +383,8 @@ white_output = 'test_videos_output/solidWhiteRight.mp4'
 ## You may also uncomment the following line for a subclip of the first 5 seconds
 ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
 clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
-white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+white_clip = clip1.fl_image(process_image)  # NOTE: this function expects color images!!
 get_ipython().run_line_magic('time', 'white_clip.write_videofile(white_output, audio=False)')
-
 
 # Play the video inline, or if you prefer find the video in your filesystem (should be in the same directory) and play it in your video player of choice.
 
@@ -307,7 +396,6 @@ HTML("""
   <source src="{0}">
 </video>
 """.format(white_output))
-
 
 # ## Improve the draw_lines() function
 # 
@@ -330,7 +418,6 @@ clip2 = VideoFileClip('test_videos/solidYellowLeft.mp4')
 yellow_clip = clip2.fl_image(process_image)
 get_ipython().run_line_magic('time', 'yellow_clip.write_videofile(yellow_output, audio=False)')
 
-
 # In[16]:
 
 
@@ -339,7 +426,6 @@ HTML("""
   <source src="{0}">
 </video>
 """.format(yellow_output))
-
 
 # ## Writeup and Submission
 # 
@@ -363,7 +449,6 @@ clip3 = VideoFileClip('test_videos/challenge.mp4')
 challenge_clip = clip3.fl_image(process_image)
 get_ipython().run_line_magic('time', 'challenge_clip.write_videofile(challenge_output, audio=False)')
 
-
 # In[18]:
 
 
@@ -372,10 +457,3 @@ HTML("""
   <source src="{0}">
 </video>
 """.format(challenge_output))
-
-
-# In[ ]:
-
-
-
-
